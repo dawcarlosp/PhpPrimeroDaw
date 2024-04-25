7e303876a4726413b2ad03ec0c16e9b17d080430
daw <dawcarlospereira@gmail.com>
2024-04-25 21:08:15 +0200

    mejora

--- model/Alumno.php
+++ model/Alumno.php
@@ -1,6 +1,7 @@
 <?php
 include 'Conexion.php';
-class Alumno {
+class Alumno
+{
     // Atributos
     public $id;
     public $codigo;
@@ -10,7 +11,8 @@ class Alumno {
     public $especialidad;
 
     // Constructor
-    public function __construct($id, $codigo, $nombre, $sexo, $edad, $especialidad) {
+    public function __construct($id, $codigo, $nombre, $sexo, $edad, $especialidad)
+    {
         $this->id = $id;
         $this->codigo = $codigo;
         $this->nombre = $nombre;
@@ -18,58 +20,83 @@ class Alumno {
         $this->edad = $edad;
         $this->especialidad = $especialidad;
     }
-    
+
     // Métodos getters y setters
-    public function getId() {
+    public function getId()
+    {
         return $this->id;
     }
 
-    public function setId($id) {
+    public function setId($id)
+    {
         $this->id = $id;
     }
 
-    public function getCodigo() {
+    public function getCodigo()
+    {
         return $this->codigo;
     }
 
-    public function setCodigo($codigo) {
+    public function setCodigo($codigo)
+    {
         $this->codigo = $codigo;
     }
 
-    public function getNombre() {
+    public function getNombre()
+    {
         return $this->nombre;
     }
 
-    public function setNombre($nombre) {
+    public function setNombre($nombre)
+    {
         $this->nombre = $nombre;
     }
 
-    public function getSexo() {
+    public function getSexo()
+    {
         return $this->sexo;
     }
 
-    public function setSexo($sexo) {
+    public function setSexo($sexo)
+    {
         $this->sexo = $sexo;
     }
 
-    public function getEdad() {
+    public function getEdad()
+    {
         return $this->edad;
     }
 
-    public function setEdad($edad) {
+    public function setEdad($edad)
+    {
         $this->edad = $edad;
     }
 
-    public function getEspecialidad() {
+    public function getEspecialidad()
+    {
         return $this->especialidad;
     }
 
-    public function setEspecialidad($especialidad) {
+    public function setEspecialidad($especialidad)
+    {
         $this->especialidad = $especialidad;
     }
+    public static function getAlumnos()
+    {
+        //Parametros
+        $server = 'localhost';
+        $user = "root";
+        $password = "toor";
+        $bbdd = 'ies';
+        $port = 3306;
+        $opciones = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"];
+        $con = new Conexion($server,$user,$password,$bbdd,$port,$opciones);
+        return $con->getFilas();
+    }
+    /*
     public static function getAlumnos(){
        global $alumnos;
        return $alumnos;
     }
+    */
 }
-?>
--- model/Conexion.php
+++ model/Conexion.php
@@ -1,26 +1,42 @@
 <?php
-//credenciales
-$server = 'localhost';
-$user = "root";
-$password = "toor";
-$bbdd = 'ies';
-$port = 3306;
-$opciones = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"];
+class Conexion
+{
+    private $server;
+    private $user;
+    private $password;
+    private $bbdd;
+    private $port;
+    private $opciones;
+    public function __construct($serverP, $userP, $passwordP, $bbddP, $portP, $opcionesP)
+    {
+        $this->server = $serverP;
+        $this->user = $userP;
+        $this->password = $passwordP;
+        $this->bbdd = $bbddP;
+        $this->port = $portP;
+        $this->opciones = $opcionesP;
+    }
+    public function getConexion()
+    {
+        try {
+            $db = new PDO('mysql:host=' . $this->server . ';dbname=' . $this->bbdd . ';port=' . $this->port, $this->user, $this->password, $this->opciones);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'Falló la conexión: ' . $e->getMessage();
+        }
 
-//Conexión mediante PDO
-//Conexión con control de errores
-try {
-    $db = new PDO('mysql:host=' . $server . ';dbname=' . $bbdd . ';port=' . $port, $user, $password, $opciones);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // Preparar consulta
-    $stmt = $db->prepare("SELECT * FROM alumnos");
+        return $db;
+    }
+    public function getFilas()
+    {
+         // Preparar consulta
+    $stmt = $this->getConexion()->prepare("SELECT * FROM alumnos");
     $stmt->execute();
 
     $alumnos = array();
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         $alumnos[] = new Alumno($row['id'], $row['codigo'], $row['nombre'], $row['sexo'], $row['edad'], $row['especialidad']);
     }
-} catch (PDOException $e) {
-    echo 'Falló la conexión: ' . $e->getMessage();
+        return $alumnos;
+    }
 }
-?>
